7bde66d6d90209cf78c3105509cdde9bdb2a9fb4
VictorGluhovskiy <vgpilot90@yandex.ru>
2022-04-27 23:29:16 +0300

    commit

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -2,7 +2,7 @@ package com.example.myapplication
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-//hw1
+//hw1hh
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
